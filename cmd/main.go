3f2a2ecfaf144fcfe558f5fe7dbb1aa334961c58
trypophob1a <trypophob1a@mail.ru>
2023-10-11 14:40:00 +0500

    worker pool queue based dynamic buffer chan

--- cmd/main.go
+++ cmd/main.go
@@ -1,130 +1,168 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"runtime"
 	"sync"
+	"sync/atomic"
+	"time"
 )
 
-type result struct {
-	Ok  int
-	Err error
+type DynamicBuffer struct {
+	lock          *sync.RWMutex
+	maxBufferSize int
+	buffer        chan Task
+	cap           int64
 }
 
-type que struct {
-	mu *sync.Mutex
-	q  []task
-}
-
-func newQue() *que {
-	return &que{
-		mu: &sync.Mutex{},
-		q:  make([]task, 0),
+func NewDynamicBuffer(initialSize, maxBufferSize int) *DynamicBuffer {
+	return &DynamicBuffer{
+		lock:          &sync.RWMutex{},
+		maxBufferSize: maxBufferSize,
+		buffer:        make(chan Task, initialSize),
+		cap:           int64(initialSize),
 	}
 }
 
-func (q *que) push(t task) {
-	q.mu.Lock()
-	defer q.mu.Unlock()
-	q.q = append(q.q, t)
-}
-
-func (q *que) pop() (task, bool) {
-	q.mu.Lock()
-	defer q.mu.Unlock()
-	if len(q.q) == 0 {
-		return nil, false
+func (db *DynamicBuffer) Add(task Task) {
+	db.lock.Lock()
+	canGrow := len(db.buffer) == cap(db.buffer) && cap(db.buffer) < db.maxBufferSize
+	if canGrow {
+		db.buffer = db.grow()
 	}
-	t := q.q[0]
-	q.q = q.q[1:]
-	return t, true
+	db.lock.Unlock()
+	db.buffer <- task
 }
 
-type task func() result
-
-type pool struct {
-	wg      *sync.WaitGroup
-	tasks   *que
-	results chan result
-	wc      int
-	ctx     context.Context
+func (db *DynamicBuffer) Cap() int {
+	db.lock.RLock()
+	defer db.lock.RUnlock()
+	return cap(db.buffer)
 }
 
-func newPool(ctx context.Context, wc int) *pool {
-	return &pool{
-		tasks:   newQue(),
-		wg:      &sync.WaitGroup{},
-		results: make(chan result),
-		wc:      wc,
-		ctx:     ctx,
-	}
+func (db *DynamicBuffer) GetCap() int64 {
+	return atomic.LoadInt64(&db.cap)
 }
 
-func (p *pool) push(t task) {
-	p.tasks.push(t)
+func (db *DynamicBuffer) Len() int {
+	db.lock.RLock()
+	defer db.lock.RUnlock()
+	return len(db.buffer)
 }
-func (p *pool) worker() error {
-	if err := p.ctx.Err(); err != nil {
-		println("cancel")
-		return err
+
+func (db *DynamicBuffer) grow() chan Task {
+	newCap := cap(db.buffer) * 2
+	if newCap > db.maxBufferSize {
+		newCap = db.maxBufferSize
 	}
 
-	t, ok := p.tasks.pop()
-	if !ok {
-		return fmt.Errorf("queue is empty")
+	db.cap = int64(newCap)
+	newBuffer := make(chan Task, newCap)
+	close(db.buffer)
+	for task := range db.buffer {
+		newBuffer <- task
 	}
-	p.results <- t()
-	return nil
+	return newBuffer
 }
 
-func (p *pool) run() {
-	for i := 0; i < p.wc; i++ {
+type Task func()
+
+type WorkerPool struct {
+	maxWorkers    int
+	waitingTasks  *DynamicBuffer
+	wg            *sync.WaitGroup
+	Len           int64
+	maxBufferSize int64
+	spaceCond     sync.Cond
+	semaphore     chan struct{}
+}
+
+func NewPool(maxWorkers, bufferSize int) *WorkerPool {
+	return &WorkerPool{
+		maxWorkers:    maxWorkers,
+		wg:            &sync.WaitGroup{},
+		waitingTasks:  NewDynamicBuffer(2, bufferSize),
+		maxBufferSize: int64(bufferSize),
+		spaceCond:     *sync.NewCond(&sync.Mutex{}),
+		semaphore:     make(chan struct{}, bufferSize),
+	}
+}
+
+func (p *WorkerPool) Run() {
+	for i := 0; i < p.maxWorkers; i++ {
 		p.wg.Add(1)
-		go func() {
-			defer p.wg.Done()
-			for {
-				err := p.worker()
-				if err != nil {
-					return
-				}
-			}
-		}()
+		go p.worker()
 	}
 }
 
-func (p *pool) wait(callback func(result result)) {
-	go func() {
-		p.wg.Wait()
-		close(p.results)
-	}()
-	for r := range p.results {
-		callback(r)
+func (p *WorkerPool) Submit(task Task) {
+	//	p.spaceCond.L.Lock()
+	//	for p.IsFull() {
+	//	p.spaceCond.Wait() // ждем оповещения
+	//		println("wait")
+	//	}
+	if p.IsFull() {
+		<-p.semaphore
+	}
+	p.waitingTasks.Add(task)
+	atomic.AddInt64(&p.Len, 1)
+	// p.spaceCond.L.Unlock()
+}
+
+func (p *WorkerPool) IsFull() bool {
+	return atomic.LoadInt64(&p.Len) == p.maxBufferSize
+}
+
+func (p *WorkerPool) hasSpace() bool {
+	return p.waitingTasks.GetCap() == p.maxBufferSize && atomic.LoadInt64(&p.Len)+1 >= p.maxBufferSize
+}
+
+func (p *WorkerPool) worker() {
+	defer p.wg.Done()
+	for {
+		task, ok := <-p.waitingTasks.buffer
+		if !ok {
+			return
+		}
+		task()
+		atomic.AddInt64(&p.Len, -1)
+		if p.hasSpace() {
+			//	p.spaceCond.Broadcast()
+			println("has space")
+			p.semaphore <- struct{}{}
+		}
 	}
 }
+
+func (p *WorkerPool) Wait() {
+	close(p.waitingTasks.buffer)
+	p.wg.Wait()
+}
+
 func main() {
-	ctx, cancel := context.WithCancel(context.Background())
+	pool := NewPool(2, 16)
 
-	p := newPool(ctx, 2)
-	p.run()
-	for i := 0; i <= 35; i++ {
-		index := i
-		p.push(func() result {
-			r := result{}
-			if index%2 == 0 {
-				r.Ok = index
-			} else {
-				r.Err = fmt.Errorf("error: %d", index)
-			}
-			return r
+	pool.Run()
+	for i := 1; i <= 5; i++ {
+		i := i
+		pool.Submit(func() {
+			println(pool.waitingTasks.Len(), pool.waitingTasks.Cap(), pool.waitingTasks.GetCap())
+			time.Sleep(2 * time.Second)
+			println("slow", i)
+			// slow <- i
+		})
+
+		pool.Submit(func() {
+			println(pool.waitingTasks.Len(), pool.waitingTasks.Cap(), pool.waitingTasks.GetCap())
+			println("fast", i)
+		})
+		pool.Submit(func() {
+			println(pool.waitingTasks.Len(), pool.waitingTasks.Cap(), pool.waitingTasks.GetCap())
+			println("fast2", i)
+		})
+		pool.Submit(func() {
+			time.Sleep(2 * time.Second)
+			println(pool.waitingTasks.Len(), pool.waitingTasks.Cap(), pool.waitingTasks.GetCap())
+			println("slow2", i)
 		})
 	}
-
-	p.wait(func(r result) {
-		fmt.Printf("ok: %v, err: %v\n", r.Ok, r.Err)
-		if r.Ok == 18 {
-			cancel()
-		}
-	})
-	fmt.Printf("end last gorutines: %d\n", runtime.NumGoroutine())
+	pool.Wait()
 }
